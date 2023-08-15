53b891da7e7bcd4e03289bb3f1e1e8be946e2253
imrenco <imrenco@MacBook-Air-de-imrenco.local>
2023-08-14 20:36:41 -0700

    fix: add types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { CoordinateTree } from "./model/CoordinateTree";
+import { SpaceObject } from "./model/SpaceObject";
 import { createSpaceObject } from "./util/http";
 import { logger } from "./util/logger";
 import { getObjectOperations } from "./util/parse";
@@ -5,8 +7,8 @@ import { getGoalCoordinates } from "./util/request";
 
 async function startCreation() {
     try {
-        const goalCoords = await getGoalCoordinates();
-        const objectOperations = getObjectOperations(goalCoords);
+        const goalCoords: CoordinateTree = await getGoalCoordinates();
+        const objectOperations: SpaceObject[] = getObjectOperations(goalCoords);
 
         // I did not make requests in parallel here because the api has a low rate limit.
         for (let index = 0; index < objectOperations.length; index++) {
